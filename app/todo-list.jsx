04624c98863d3ef8b4f6c27e7841e15472e53f42
Vinicios Neves <vinicios.neves@mac>
2025-01-28 16:35:03 +0000

    video 2.3

--- app/todo-list.jsx
+++ app/todo-list.jsx
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native"
+import { FokusButton } from "../components/FokusButton"
+import { router } from "expo-router"
+import { IconPlus } from "../components/Icons"
 
-export default function Pomodoro() {
+export default function TooList() {
+
     return (
-        <View>
-            <Text>
-                Lista de tarefas
-            </Text>
+        <View
+            style={styles.container}
+        >
+
+            <FokusButton 
+                title="Adicionar nova tarefa" 
+                onPress={() => router.push('/todo-form.jsx')} 
+                icon={<IconPlus />}
+                outline
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#021123'
+    }
+})
--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,29 +1,48 @@
-import { Pressable, StyleSheet, Text } from "react-native"
+import { Pressable, StyleSheet, Text } from "react-native";
 
-export const FokusButton = ({ onPress, title, icon }) => {
+export const FokusButton = ({ onPress, title, icon, outline = false }) => {
     return (
-        <Pressable style={styles.button} onPress={onPress}>
+        <Pressable
+            style={[
+                styles.button,
+                outline && styles.outlineButton
+            ]}
+            onPress={onPress}
+        >
             {icon}
-            <Text style={styles.buttonText}>
+            <Text
+                style={[
+                    styles.buttonText,
+                    outline && styles.outlineButtonText
+                ]}
+            >
                 {title}
             </Text>
         </Pressable>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#B872FF',
-    borderRadius: 32,
-    padding: 8,
-    flexDirection: 'row',
-    gap: 12,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: '#021123',
-    fontSize: 18
-  }
-})
+    button: {
+        backgroundColor: '#B872FF',
+        borderRadius: 32,
+        padding: 8,
+        flexDirection: 'row',
+        gap: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    outlineButton: {
+        backgroundColor: 'transparent',
+        borderWidth: 2,
+        borderColor: '#B872FF',
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#021123',
+        fontSize: 18,
+    },
+    outlineButtonText: {
+        color: '#B872FF',
+    },
+});
