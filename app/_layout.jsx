ba9b9750c213c691ccefe05d45b39f8de7e03f0f
Vinicios Neves <vinicios.neves@mac>
2025-01-28 15:53:13 +0000

    video 2.2

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,14 +4,36 @@ import { Drawer } from 'expo-router/drawer';
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer 
+      <Drawer
         screenOptions={{
           headerStyle: { backgroundColor: '#021123' },
           headerTintColor: '#FFF',
           drawerStyle: { backgroundColor: '#021123' },
           drawerLabelStyle: { color: "#FFF" },
         }}
-      />
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerItemStyle: { display: "none" },
+            headerShown: false,
+          }}
+        />
+        <Drawer.Screen
+          name="pomodoro"
+          options={{
+            drawerLabel: "Timer",
+            title: ""
+          }}
+        />
+        <Drawer.Screen
+          name="todo-list"
+          options={{
+            drawerLabel: "Lista de terfas",
+            title: ""
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 }
